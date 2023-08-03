35c873c24e1f32381e89acb69c70f9798a057507
Michael Yang <mxyng@pm.me>
2023-08-03 15:40:16 -0700

    disable gpu for q5_0, q5_1, q8_0 quants

--- llm/llm.go
+++ llm/llm.go
@@ -2,6 +2,7 @@ package llm
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/jmorganca/ollama/api"
@@ -25,6 +26,14 @@ func New(model string, opts api.Options) (LLM, error) {
 	ggml, err := DecodeGGML(f, ModelFamilyLlama)
 	if err != nil {
 		return nil, err
+	}
+
+	switch ggml.FileType {
+	case FileTypeQ5_0, FileTypeQ5_1, FileTypeQ8_0:
+		// Q5_0, Q5_1, and Q8_0 do not support Metal API and will
+		// cause the runner to segmentation fault so disable GPU
+		log.Printf("WARNING: GPU disabled for Q5_0, Q5_1, and Q8_0")
+		opts.NumGPU = 0
 	}
 
 	switch ggml.ModelFamily {
