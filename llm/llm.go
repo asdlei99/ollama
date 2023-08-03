353121c9dcc97cf66e0eae641834cb5920387535
Michael Yang <mxyng@pm.me>
2023-08-03 15:47:36 -0700

    check memory requirements before loading

--- llm/llm.go
+++ llm/llm.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 
+	"github.com/pbnjay/memory"
+
 	"github.com/jmorganca/ollama/api"
 )
 
@@ -34,6 +36,26 @@ func New(model string, opts api.Options) (LLM, error) {
 		// cause the runner to segmentation fault so disable GPU
 		log.Printf("WARNING: GPU disabled for Q5_0, Q5_1, and Q8_0")
 		opts.NumGPU = 0
+	}
+
+	totalResidentMemory := memory.TotalMemory()
+	switch ggml.ModelType {
+	case ModelType3B, ModelType7B:
+		if totalResidentMemory < 8*1024*1024 {
+			return nil, fmt.Errorf("model requires at least 8GB of memory")
+		}
+	case ModelType13B:
+		if totalResidentMemory < 16*1024*1024 {
+			return nil, fmt.Errorf("model requires at least 16GB of memory")
+		}
+	case ModelType30B:
+		if totalResidentMemory < 32*1024*1024 {
+			return nil, fmt.Errorf("model requires at least 32GB of memory")
+		}
+	case ModelType65B:
+		if totalResidentMemory < 64*1024*1024 {
+			return nil, fmt.Errorf("model requires at least 64GB of memory")
+		}
 	}
 
 	switch ggml.ModelFamily {
